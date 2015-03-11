55ed05e14342a8ffdf9cd00f6c25b216384a5e80
MercAddons <malbamoyano@gmail.com>
2015-03-11 01:15:06 +0100

    cmdfind ALPHA

---
+++ cmdfind.rb
@@ -0,0 +1,49 @@
+#VERSION ALPHA
+#This project is under GNU GPL v3
+#author: MercAddons
+
+require 'multimap'
+
+map=Multimap.new
+map["contenido"]="ls"
+map["contenido"]="cat"
+map["listar"]="ls"
+map["directorios"]="ls"
+map["directorios"]="cp"
+map["directorios"]="rm"
+map["directorios"]="mkdir"
+map["directorios"]="rmdir"
+map["directorios"]="chmod"
+map["directorios"]="chown"
+map["directorios"]="chgrp"
+map["copiar"]="cp"
+map["ficheros"]="cp"
+map["ficheros"]="rm"
+map["ficheros"]="mv"
+map["ficheros"]="cat"
+map["ficheros"]="chmod"
+map["ficheros"]="chown"
+map["ficheros"]="chgrp"
+map["eliminar"]="rm"
+map["borrar"]="rm"
+map["borrar"]="rmdir"
+map["crear"]="mkdir"
+map["mover"]="mv"
+map["renombrar"]="mv"
+map["cambiar"]="mv"
+map["cambiar"]="chmod"
+map["cambiar"]="chown"
+map["cambiar"]="chgrp"
+map["nombre"]="mv"
+map["mostrar"]="ls"
+map["mostrar"]="cat"
+map["concatenar"]="cat"
+map["visualizar"]="cat"
+map["acceso"]="chmod"
+map["usuario"]="chown"
+map["grupo"]="chown"
+map["grupo"]="chagrp"
+map["propietario"]="chown"
+map["pertenecer"]="chgrp"
+
+map
