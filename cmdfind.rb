b216a02597da06e2ad48b391702c8728e4ecf90a
MercAddons <malbamoyano@gmail.com>
2015-03-13 01:59:01 +0100

    cmdfind 0.1

--- cmdfind.rb
+++ cmdfind.rb
@@ -1,16 +1,58 @@
 #!/usr/bin/env ruby
 #VERSION ALPHA
-#This project is under GNU GPL v3
+#This project is under GNU GPL v3#author: MercAddons
 
-=begin Archivo main 
-=end
+
+require 'multimap'
+
+map=Multimap.new
+map["contenido"]="cat"
+map["contenido"]="ls"
+map["listar"]="ls"
+map["directorios"]="ls"
+map["directorios"]="cp"
+map["directorios"]="rm"
+map["directorios"]="mkdir"
+map["directorios"]="rmdir"
+map["directorios"]="chmod"
+map["directorios"]="chown"
+map["directorios"]="chgrp"
+map["copiar"]="cp"
+map["ficheros"]="cp"
+map["ficheros"]="rm"
+map["ficheros"]="mv"
+map["ficheros"]="cat"
+map["ficheros"]="chmod"
+map["ficheros"]="chown"
+map["ficheros"]="chgrp"
+map["eliminar"]="rm"
+map["eliminar"]="rmdir"
+map["borrar"]="rm"
+map["borrar"]="rmdir"
+map["crear"]="mkdir"
+map["mover"]="mv"
+map["renombrar"]="mv"
+map["cambiar"]="mv"
+map["cambiar"]="chmod"
+map["cambiar"]="chown"
+map["cambiar"]="chgrp"
+map["nombre"]="mv"
+map["mostrar"]="ls"
+map["mostrar"]="cat"
+map["concatenar"]="cat"
+map["visualizar"]="cat"
+map["acceso"]="chmod"
+map["usuario"]="chown"
+map["grupo"]="chown"
+map["grupo"]="chgrp"
+map["propietario"]="chown"
+map["pertenecer"]="chgrp"
 
 #Entrada del string
 puts "Buscar comando: "  
 STDOUT.flush  
 frase = gets.chomp  
 frase.downcase!			#Cambiar carácteres del string a minúsculas
-
 
 *claves=frase.split(" ")	#Separar el string por palabras
 *comandos=map.values_at(*claves)
